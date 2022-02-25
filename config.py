779f324950db46db5b1b7e40de6fc00d7bfa4238
Roman3214 <87423013+Roman3214@users.noreply.github.com>
2022-02-25 20:12:15 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN = "4ebcf06bb7a7dc35-660577619c3cc428-b4d38da3cd44b45"# viber
-NGROK_TOKEN = "253z9xQQiT3LR5G7rsqsIh26tGL_2hbJzYVbUr4Vgdq5VS6mN"
-#./ngrok -authtoken 253z9xQQiT3LR5G7rsqsIh26tGL_2hbJzYVbUr4Vgdq5VS6mN 8887
-#curl -# -i -g -H "X-Viber-Auth-Token: 4ebcf06bb7a7dc35-660577619c3cc428-b4d38da3cd44b45" -d @viber.json -X POST https://chatapi.viber.com/pa/set_webhook -v
-
-
-#ngrok authtoken 253z9xQQiT3LR5G7rsqsIh26tGL_2hbJzYVbUr4Vgdq5VS6mN
-#'https://viber.com/botforimportantnotifications'
