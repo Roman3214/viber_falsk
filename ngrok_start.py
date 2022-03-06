0887d112036cdd33a76b725859af694d4336cd5f
Roman3214 <87423013+Roman3214@users.noreply.github.com>
2022-03-06 18:51:30 +0300

    Update ngrok_start.py

--- ngrok_start.py
+++ ngrok_start.py
@@ -1,41 +1,10 @@
 import os
-
 import subprocess
-import logging
+import time
 
-import json
-def get_ngrok_address():
-    os.system("curl  http://localhost:4040/api/tunnels > tunnels.json")
-
-    with open('tunnels.json') as data_file:
-        datajson = json.load(data_file)
-        
-    with open('tunnels.json', 'r') as file_json:
-        respone_dict = file_json.read()
-
-    begin = str(respone_dict).find('https:')
-    enD = str(respone_dict).find('.io","proto"')
-    URL = str(respone_dict)[begin : enD+3]
-    sTr = ['{\n\t"url": "', '"\n}']
-    file_json_viber = URL.join(sTr)
-    
-    with open('viber.json', 'w') as file:
-        file.write(str(file_json_viber))
-    return dict(zip(['http', 'https'], [i['public_url'] for i in datajson['tunnels']])) 
-
-
-print(logging.debug("This is a debug message"))
-print(logging.info("Informational message"))
-logging.error("An error has happened!")
-
-
-
-
-files = ["new2test.py", "new.py"]  # файлы, которые нужно запустить
+files = [ "new2test.py","url.py", "lol.py"]  # файлы, которые нужно запустить
 for file in files:
     subprocess.Popen(args=["start", "python", file], shell=True, stdout=subprocess.PIPE)
-
-
-get_ngrok_address()
+    time.sleep(5)
 
 os.system ('curl -# -i -g -H "X-Viber-Auth-Token: "<you_auto_token>" -d @viber.json -X POST https://chatapi.viber.com/pa/set_webhook -v')
